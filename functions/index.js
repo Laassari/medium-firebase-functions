3cbc3229876edc1dc85ab4c6e2c7e00528437356
Laassari <nlaassari@gmail.com>
2018-10-18 04:38:27 +0100

    add updatePostClaps function

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,27 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
-const config = {
-  apiKey: 'AIzaSyD3H-hAKV5VFKTeehFTc9EnAlfNV4lDsMg',
-  authDomain: 'meduim-clone.firebaseapp.com',
-  databaseURL: 'https://meduim-clone.firebaseio.com',
-  projectId: 'meduim-clone',
-  storageBucket: 'meduim-clone.appspot.com',
-  messagingSenderId: '978295741901',
-}
+admin.initializeApp(functions.config().firebase)
+const db = admin.firestore()
+firestore.settings({ timestampsInSnapshots: true })
 
-admin.initializeApp(config)
+/* 
+  claps is the number of keys in a doc for a specific
+  post. eg:
+  /likes/postId/{object if IDs}
+  the number of ids is the number of claps
+  update the claps whenever a key is added/removed
+*/
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.updatePostClaps = functions.firestore
+  .document('likes/{postId}')
+  .onWrite((change, context) => {
+    const { postId } = context.params
+
+    const data = change.after.data()
+    const claps = Object.keys(data).length
+
+    const postRef = db.collection('posts').doc(postId)
+
+    return postRef.update({ claps })
+  })
